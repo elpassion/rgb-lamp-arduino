d03ab23f89df0eef63c351e0f4c8eac7a511eb6e
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2019-01-11 14:04:57 +0100

    Version 1.0

--- lampa.ino
+++ lampa.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <Rotary.h>
 
 #define RED_LED 6
 #define GREEN_LED 5
@@ -7,9 +8,30 @@
 #define BLUETOOTH_TX 8
 #define BLUETOOTH_RX 7
 
+#define HUE_ROTARY_LEFT 12
+#define HUE_ROTARY_RIGHT 13
+
+Rotary hueRotary = Rotary(HUE_ROTARY_LEFT, HUE_ROTARY_RIGHT);
+#define HUE_JUMP 0.05
+
+#define ROTARY_LEFT 11
+#define ROTARY_RIGHT 10
+
+Rotary saturationRotary = Rotary(ROTARY_LEFT, ROTARY_RIGHT);
+#define SATURATION_JUMP 0.05
+
+#define VALUE_DECREASE_PIN 4
+#define VALUE_INCREASE_PIN 2
+#define VALUE_JUMP 0.05
+
+#define INIT_DELAY_TIME 500
+
+#define VALUE_DIFF_DELAY 100
+
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
 
-//  Hue|Saturation|Value|
+// Bluetooth data format:
+// Hue|Saturation|Value|
 // [0.0-1.0]|[0.0-1.0]|[0.0-1.0]|
 // 0.52|0.43|0.95|
 
@@ -27,61 +49,107 @@ struct rgbColor {
   float blue;
 };
 
-hsvColor lampColor = { 0.4, 1.0, 1.0 };
+hsvColor lampColor = { 0.32, 1.0, 1.0 };
 
 void setup() {
+  changeLampColor({1.0, 0.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 1.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 0.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 1.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 0.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 1.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 1.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  updateLamp();
+
   Serial.begin(9600);
   bluetooth.begin(9600);
-  int delayTime = 1000;
 
-  updateLamp({1.0, 0.0, 0.0});
-  delay(delayTime);
+  pinMode(VALUE_DECREASE_PIN, INPUT_PULLUP);
+  pinMode(VALUE_INCREASE_PIN, INPUT_PULLUP);
 
-  updateLamp({0.0, 1.0, 0.0});
-  delay(delayTime);
-
-  updateLamp({0.0, 0.0, 1.0});
-  delay(delayTime);
-
-  updateLamp({1.0, 1.0, 0.0});
-  delay(delayTime);
-
-  updateLamp({1.0, 0.0, 1.0});
-  delay(delayTime);
-
-  updateLamp({0.0, 1.0, 1.0});
-  delay(delayTime);
-
-  updateLamp({1.0, 1.0, 1.0});
-  delay(delayTime);
-
-  updateLamp({0.5, 0.5, 1.0});
-  delay(delayTime);
+  hueRotary.begin(true);
+  saturationRotary.begin(true);
 }
 
 void loop() {
-  return;
-  if (bluetooth.available() < 1) return;
+  int valueIncrease = digitalRead(VALUE_INCREASE_PIN);
+  int valueDecrease = digitalRead(VALUE_DECREASE_PIN);
 
-  float hue = bluetooth.parseFloat();
-  if (bluetooth.read() != SEP_CMD_CHAR) return;
+  unsigned char hueResult = hueRotary.process();
+  unsigned char saturationResult = saturationRotary.process();
 
-  float saturation = bluetooth.parseFloat();
-  if (bluetooth.read() != SEP_CMD_CHAR) return;
-
-  float value = bluetooth.parseFloat();
-  lampColor = { hue, saturation, value };
-
-//  lampColor.hue += 0.05;
-//  if (lampColor.hue >= 1.0) {
-//    lampColor.hue = 0.0;
-//  } 
-//  delay(1000);
-
-  updateLamp(rgbFromHsv(lampColor));
+  if (valueDecrease == LOW) {
+    addLimitedDiff(&lampColor.value, -VALUE_JUMP);
+    delay(VALUE_DIFF_DELAY);
+  } else if (valueIncrease == LOW) {
+    addLimitedDiff(&lampColor.value, VALUE_JUMP);
+    delay(VALUE_DIFF_DELAY);
+  } else if (hueResult) {
+    if (hueResult == DIR_CW) {
+      addRoundingDiff(&lampColor.hue, -HUE_JUMP);
+    } else {
+      addRoundingDiff(&lampColor.hue, HUE_JUMP);
+    }
+  } else if (saturationResult) {
+    if (saturationResult == DIR_CW) {
+      addLimitedDiff(&lampColor.saturation, -SATURATION_JUMP);
+    } else {
+      addLimitedDiff(&lampColor.saturation, SATURATION_JUMP);
+    }
+  } else if(bluetooth.available() > 1) {
+    float hue = bluetooth.parseFloat();
+    if (bluetooth.read() != SEP_CMD_CHAR) return;
+  
+    float saturation = bluetooth.parseFloat();
+    if (bluetooth.read() != SEP_CMD_CHAR) return;
+  
+    float value = bluetooth.parseFloat();
+    lampColor = { hue, saturation, value };
+  } else {
+    return;
+  }
+  updateLamp();
 }
 
-void updateLamp(rgbColor rgb) {
+void addRoundingDiff(float* value, float diff) {
+  float newValue = *value + diff;
+  if (newValue < 0) {
+    newValue = 1.0;
+  } else if (newValue > 1.0) {
+    newValue = 0.0;
+  }
+  *value = newValue;
+}
+
+void addLimitedDiff(float* value, float diff) {
+  float newValue = *value + diff;
+  if (newValue < 0) {
+    newValue = 0.0;
+  } else if (newValue > 1.0) {
+    newValue = 1.0;
+  }
+  *value = newValue;
+}
+
+void updateLamp() {
+  changeLampColor(rgbFromHsv(lampColor));
+}
+
+void changeLampColor(rgbColor rgb) {
   Serial.println("Update RGB");
   Serial.println((int)(rgb.red * 255));
   Serial.println((int)(rgb.green * 255));
