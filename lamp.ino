dfa82c8adcbe90dbe962eb5fa37f95f28ec70e00
maciejmatu <maciek.matuszewski@gmail.com>
2019-01-31 19:28:31 +0100

    Update pins for ATMega. cleanup code

--- lamp.ino
+++ lamp.ino
@@ -1,39 +1,36 @@
+/* 
+Bluetooth data format:
+Hue|Saturation|Value|
+[0.0-1.0]|[0.0-1.0]|[0.0-1.0]|
+0.52|0.43|0.95|
+*/
+
 #include <SoftwareSerial.h>
 #include <Rotary.h>
 
-#define RED_LED 6
-#define GREEN_LED 5
-#define BLUE_LED 3
+#define LED_RED 9
+#define LED_GREEN 10
+#define LED_BLUE 11
 
-#define BLUETOOTH_TX 8
-#define BLUETOOTH_RX 7
-
-#define HUE_ROTARY_LEFT 12
-#define HUE_ROTARY_RIGHT 13
-
-Rotary hueRotary = Rotary(HUE_ROTARY_LEFT, HUE_ROTARY_RIGHT);
+#define HUE_ROTARY_RIGHT 6
+#define HUE_ROTARY_LEFT 7
 #define HUE_JUMP 0.05
 
-#define ROTARY_LEFT 11
-#define ROTARY_RIGHT 10
-
-Rotary saturationRotary = Rotary(ROTARY_LEFT, ROTARY_RIGHT);
+#define SATURATION_ROTARY_RIGHT 2
+#define SATURATION_ROTARY_LEFT 3
 #define SATURATION_JUMP 0.05
 
-#define VALUE_DECREASE_PIN 4
-#define VALUE_INCREASE_PIN 2
+#define VALUE_DECREASE_PIN 8
+#define VALUE_INCREASE_PIN 4
 #define VALUE_JUMP 0.05
 
 #define INIT_DELAY_TIME 500
-
 #define VALUE_DIFF_DELAY 100
 
-SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
+Rotary hueRotary = Rotary(HUE_ROTARY_LEFT, HUE_ROTARY_RIGHT);
+Rotary saturationRotary = Rotary(SATURATION_ROTARY_LEFT, SATURATION_ROTARY_RIGHT);
 
-// Bluetooth data format:
-// Hue|Saturation|Value|
-// [0.0-1.0]|[0.0-1.0]|[0.0-1.0]|
-// 0.52|0.43|0.95|
+SoftwareSerial bluetooth(0, 1);
 
 char SEP_CMD_CHAR = '|';
 
@@ -49,40 +46,17 @@ struct rgbColor {
   float blue;
 };
 
-hsvColor lampColor = { 0.32, 1.0, 1.0 };
+hsvColor lampColor = {0.32, 1.0, 1.0};
 
 void setup() {
-  changeLampColor({1.0, 0.0, 0.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({0.0, 1.0, 0.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({0.0, 0.0, 1.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({0.0, 1.0, 1.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({1.0, 0.0, 1.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({1.0, 1.0, 0.0});
-  delay(INIT_DELAY_TIME);
-
-  changeLampColor({1.0, 1.0, 1.0});
-  delay(INIT_DELAY_TIME);
-
-  updateLamp();
-
-  Serial.begin(9600);
+  startupSequence();
+  
   bluetooth.begin(9600);
-
-  pinMode(VALUE_DECREASE_PIN, INPUT_PULLUP);
-  pinMode(VALUE_INCREASE_PIN, INPUT_PULLUP);
-
   hueRotary.begin(true);
   saturationRotary.begin(true);
+  
+  pinMode(VALUE_DECREASE_PIN, INPUT_PULLUP);
+  pinMode(VALUE_INCREASE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -110,38 +84,69 @@ void loop() {
     } else {
       addLimitedDiff(&lampColor.saturation, SATURATION_JUMP);
     }
-  } else if(bluetooth.available() > 1) {
+  } else if (bluetooth.available() > 1) {
     float hue = bluetooth.parseFloat();
-    if (bluetooth.read() != SEP_CMD_CHAR) return;
-  
+    if (bluetooth.read() != SEP_CMD_CHAR)
+      return;
+
     float saturation = bluetooth.parseFloat();
-    if (bluetooth.read() != SEP_CMD_CHAR) return;
-  
+    if (bluetooth.read() != SEP_CMD_CHAR)
+      return;
+
     float value = bluetooth.parseFloat();
-    lampColor = { hue, saturation, value };
+    lampColor = {hue, saturation, value};
   } else {
     return;
   }
+  
   updateLamp();
 }
 
-void addRoundingDiff(float* value, float diff) {
+void startupSequence() {
+  changeLampColor({1.0, 0.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 1.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 0.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({0.0, 1.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 0.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 1.0, 0.0});
+  delay(INIT_DELAY_TIME);
+
+  changeLampColor({1.0, 1.0, 1.0});
+  delay(INIT_DELAY_TIME);
+
+  updateLamp();
+}
+
+void addRoundingDiff(float *value, float diff) {
   float newValue = *value + diff;
+  
   if (newValue < 0) {
     newValue = 1.0;
   } else if (newValue > 1.0) {
     newValue = 0.0;
   }
+  
   *value = newValue;
 }
 
-void addLimitedDiff(float* value, float diff) {
+void addLimitedDiff(float *value, float diff) {
   float newValue = *value + diff;
   if (newValue < 0) {
     newValue = 0.0;
   } else if (newValue > 1.0) {
     newValue = 1.0;
   }
+  
   *value = newValue;
 }
 
@@ -150,14 +155,9 @@ void updateLamp() {
 }
 
 void changeLampColor(rgbColor rgb) {
-  Serial.println("Update RGB");
-  Serial.println((int)(rgb.red * 255));
-  Serial.println((int)(rgb.green * 255));
-  Serial.println((int)(rgb.blue * 255));
-
-  analogWrite(RED_LED, (int)(rgb.red * 255));
-  analogWrite(GREEN_LED, (int)(rgb.green * 255));
-  analogWrite(BLUE_LED, (int)(rgb.blue * 255));
+  analogWrite(LED_RED, (int)(rgb.red * 255));
+  analogWrite(LED_GREEN, (int)(rgb.green * 255));
+  analogWrite(LED_BLUE, (int)(rgb.blue * 255));
 }
 
 // https://gist.github.com/postspectacular/2a4a8db092011c6743a7
@@ -170,5 +170,5 @@ rgbColor rgbFromHsv(hsvColor hsv) {
   float green = hsv.value * mix(1.0, constrain(fabs(fract(hsv.hue + 0.6666666) * 6.0 - 3.0) - 1.0, 0.0, 1.0), hsv.saturation);
   float blue = hsv.value * mix(1.0, constrain(fabs(fract(hsv.hue + 0.3333333) * 6.0 - 3.0) - 1.0, 0.0, 1.0), hsv.saturation);
 
-  return { red, green, blue };
+  return {red, green, blue};
 }
